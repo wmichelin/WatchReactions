b4bf1d67075e82226e0ca0fd6e1531f7978b186b
Walter Michelin <wmichelin@gmail.com>
2019-12-24 13:47:47 -0700

    Initial Commit

---
+++ WatchReactions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchReactions
+//
+//  Created by Walter Michelin on 12/24/19.
+//  Copyright © 2019 Walter Michelin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
